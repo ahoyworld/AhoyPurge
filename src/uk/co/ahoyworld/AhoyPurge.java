4b50a67f374e27e5da155372a15b3ba5d7a0d9eb
Jack Williams <jpwilliams@ahoyworld.co.uk>
2012-05-29 23:16:36 +0100

    AhoyPurge code

---
+++ src/uk/co/ahoyworld/AhoyPurge.java
@@ -0,0 +1,38 @@
+package uk.co.ahoyworld;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AhoyPurge extends JavaPlugin
+{
+	Logger log;
+	
+	public void onEnable()
+	{
+		log = this.getLogger();
+		log.info("Plugin has been enabled.");
+	}
+	
+	public void onDisable()
+	{
+		log.info("Plugin has been disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+	{
+		Player player = (Player) sender;
+		
+		if (cmd.getName().equalsIgnoreCase("purge"))
+		{
+			player.setFlying(true);
+			player.sendMessage("You ran a command. Fuck you.");
+			return true;
+		}
+		
+		return false;
+	}
+}
